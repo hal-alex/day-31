0b27f3d1c43601323bf045ab8d57d34bc0e3bd31
hal-alex <102769562+hal-alex@users.noreply.github.com>
2022-04-16 19:18:11 +0100

    French/English Word Guessing Game

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 word_data = pandas.read_csv("./data/french_words.csv")
 word_dict = word_data.to_dict(orient="records")
+random_word = {}
 
 print(word_dict)
 
@@ -21,13 +22,21 @@ def reject():
     generate_new_word()
 
 def generate_new_word():
-
+    global random_word, window_timer
+    window.after_cancel(window_timer)
     random_word = random.choice(word_dict)
     french_word = random_word["French"]
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=french_word)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=french_word, fill="black")
+    canvas.itemconfig(card_background, image=flash_card_front_image)
+    window.after(3000, func=back_of_card)
 
+def back_of_card():
 
+    english_word = random_word["English"]
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=english_word, fill="white")
+    canvas.itemconfig(card_background, image=flash_card_back_image)
 
 
 
@@ -37,9 +46,12 @@ window = Tk()
 window.title("French/English Word Guessing Game")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+window_timer = window.after(3000, func=back_of_card)
+
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 flash_card_front_image = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=flash_card_front_image)
+flash_card_back_image = PhotoImage(file="./images/card_back.png")
+card_background = canvas.create_image(400, 263, image=flash_card_front_image)
 canvas.grid(column=0, row=0, columnspan=2)
 
 approve_image = PhotoImage(file="./images/right.png")
@@ -54,5 +66,6 @@ card_title = canvas.create_text(400, 150, text="French", font=("Arial", 30, "ita
 card_word = canvas.create_text(400, 250, text="Word", font=("Arial", 42, "bold"))
 
 generate_new_word()
+
 
 window.mainloop()
